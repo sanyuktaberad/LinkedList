26253f72b0d4704a1b3d256ebfb63e900be3e2cb
sanyuktaberad <sanyukta.berad@gmail.com>
2023-06-26 14:04:59 +0530

    Add : Code for reverse Linked List is added.

--- src/com/bridgelabz/DemoLinkedList.java
+++ src/com/bridgelabz/DemoLinkedList.java
@@ -6,10 +6,15 @@ import java.util.List;
 public class DemoLinkedList {
     public static void main(String[] args) {
         List<Integer> numbers = new LinkedList<>();
-        numbers.add(56);
-        numbers.add(30);
         numbers.add(70);
+        numbers.add(30);
+        numbers.add(56);
 
-        System.out.println(numbers);
+        List<Integer> reverseOrder = new LinkedList<>();
+
+        for (int i = numbers.size()-1; i >= 0 ; i--){
+            reverseOrder.add(numbers.get(i));
+        }
+        System.out.println(reverseOrder);
     }
 }
