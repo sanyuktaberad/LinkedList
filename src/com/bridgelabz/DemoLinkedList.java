ba810ccefa4ce380871b503ca80836b151b4e61d
sanyuktaberad <sanyukta.berad@gmail.com>
2023-06-26 15:54:47 +0530

    Add: Code for searching node with value in linked list is added.

--- src/com/bridgelabz/DemoLinkedList.java
+++ src/com/bridgelabz/DemoLinkedList.java
@@ -1,7 +1,6 @@
 package com.bridgelabz;
 
 import java.util.LinkedList;
-import java.util.List;
 
 public class DemoLinkedList {
     public static void main(String[] args) {
@@ -10,8 +9,10 @@ public class DemoLinkedList {
         numbers.add(30);
         numbers.add(70);
         System.out.println(numbers);
-
-        numbers.pollLast();
-        System.out.println(numbers);
+        for (int i =0; i < numbers.size()-1;i++ ){
+            if (numbers.get(i)!= 0 && numbers.get(i) == 30){
+                        System.out.println(numbers.get(i-1)+ " is node and value is  "+numbers.get(i));
+            }
+        }
     }
 }
