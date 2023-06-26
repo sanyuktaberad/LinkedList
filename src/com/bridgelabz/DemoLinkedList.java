67fbafaad243bd3a9111a986aebc6445e49d868a
sanyuktaberad <sanyukta.berad@gmail.com>
2023-06-26 16:01:04 +0530

    Add: Code for adding element in between is added.

--- src/com/bridgelabz/DemoLinkedList.java
+++ src/com/bridgelabz/DemoLinkedList.java
@@ -9,10 +9,8 @@ public class DemoLinkedList {
         numbers.add(30);
         numbers.add(70);
         System.out.println(numbers);
-        for (int i =0; i < numbers.size()-1;i++ ){
-            if (numbers.get(i)!= 0 && numbers.get(i) == 30){
-                        System.out.println(numbers.get(i-1)+ " is node and value is  "+numbers.get(i));
-            }
-        }
+
+        numbers.add(2,40);
+        System.out.println(numbers);
     }
 }
