e667583ec836a736bdeacab636b92b986d4209dc
sanyuktaberad <sanyukta.berad@gmail.com>
2023-06-26 13:38:55 +0530

    Add: Code for creating linked list is added.

---
+++ src/com/bridgelabz/DemoLinkedList.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class DemoLinkedList {
+    public static void main(String[] args) {
+        List<Integer> numbers = new LinkedList<>();
+        numbers.add(56);
+        numbers.add(30);
+        numbers.add(70);
+
+        System.out.println(numbers);
+    }
+}
