c5524591f80210c4db703e668ccbe086003c721f
sanyuktaberad <sanyukta.berad@gmail.com>
2023-06-26 14:29:02 +0530

    Add: Code for deleting first eleent is added.

--- src/com/bridgelabz/DemoLinkedList.java
+++ src/com/bridgelabz/DemoLinkedList.java
@@ -5,12 +5,13 @@ import java.util.List;
 
 public class DemoLinkedList {
     public static void main(String[] args) {
-        List<Integer> numbers = new LinkedList<>();
+        LinkedList<Integer> numbers = new LinkedList<>();
         numbers.add(56);
+        numbers.add(30);
         numbers.add(70);
         System.out.println(numbers);
 
-        numbers.add(1,30);
+        numbers.pop();
         System.out.println(numbers);
 
 
